f654cb75decb53620bf74ff3377969af252d017d
Yeonjae Woo <yeonjae0331@naver.com>
2016-05-24 18:55:34 +0900

    #0 New Project

---
+++ app/src/main/java/kr/hs/emirim/yeonjae0331/viewflippertest/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.yeonjae0331.viewflippertest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
